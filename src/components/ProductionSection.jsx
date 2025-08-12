97ea014896155646bdf1ec88ce20daae495a00b5
mikisz <mikolaj.niznik@gmail.com>
2025-08-12 18:05:22 +0200

    feat: add building row and production section

---
+++ src/components/ProductionSection.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Accordion from './Accordion.jsx';
+import BuildingRow from './BuildingRow.jsx';
+
+export default function ProductionSection({
+  productionGroups = [],
+  storageBuildings = [],
+  completedResearch = [],
+}) {
+  return (
+    <div className="border border-stroke rounded">
+      {productionGroups.map((group) => (
+        <Accordion key={group.name} title={group.name} defaultOpen>
+          {group.buildings.map((b) => (
+            <BuildingRow
+              key={b.id}
+              building={b}
+              completedResearch={completedResearch}
+            />
+          ))}
+        </Accordion>
+      ))}
+      <Accordion title="Storage">
+        {storageBuildings.map((b) => (
+          <BuildingRow
+            key={b.id}
+            building={b}
+            completedResearch={completedResearch}
+          />
+        ))}
+      </Accordion>
+    </div>
+  );
+}
+
